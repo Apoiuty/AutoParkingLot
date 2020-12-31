8f1017cdad75e88af623a3bc68c809475f7723f7
Truth <tideborn@foxmail.com>
2020-12-31 22:03:47 +0800

    新数据库操作测试

---
+++ DB/initialize.py
@@ -0,0 +1,36 @@
+from DB.operation import DatabaseVisitor
+import sqlite3
+#开发时使用创建
+if __name__=='__main__':
+
+    conn = sqlite3.connect('./Database.db')
+    cursor = conn.cursor()
+    cursor.close()
+    conn.commit()
+    conn.close()
+    dbvisitor=DatabaseVisitor('./Database.db')
+    sql='''CREATE TABLE House
+       (Hcar VARCHAR(10) PRIMARY KEY ,
+       Howner VARCHAR(10), 
+       Hplace VARCHAR(10), 
+       Hownerkind VARCHAR(10))'''
+    dbvisitor.create_table(sql)
+    sql='''CREATE TABLE Social
+       (Scar VARCHAR(10) PRIMARY KEY ,
+       Sin DATETIME, 
+       Sout DATETIME, 
+       Sfee FLOAT,
+       Srate FLOAT)'''
+    dbvisitor.create_table(sql)
+    sql='''CREATE TABLE User
+       (Uname VARCHAR(10) PRIMARY KEY ,
+       Upassword VARCHAR(10), 
+       Urank INT, 
+       Uphone VARCHAR(15))'''
+    dbvisitor.create_table(sql)
+    sql='''INSERT INTO User (Uname,Upassword,Urank,Uphone)
+            VALUES ('worker01', 'password', 5, '12932277777')'''
+    dbvisitor.update(sql)
+    sql="SELECT * FROM User"
+    result=dbvisitor.find_all(sql)
+    print(result)
---
+++ DB/operation.py
@@ -0,0 +1,76 @@
+import sqlite3
+
+# 数据库访问类
+class DatabaseVisitor():
+    def __init__(self, db):
+        self._conn = None
+        self._cursor = None
+        self._db = db
+
+    # 连接数据库
+    def connect(self):
+        self._conn = sqlite3.connect(self._db)
+        self._cursor = self._conn.cursor()
+
+    # 关闭游标和连接
+    def close(self):
+        self._cursor.close()
+        self._conn.close()
+
+    # 创建表
+    def create_table(self, sql):
+        self.connect()
+        try:
+            self._cursor.execute(sql)  # 游标执行
+            self._conn.commit()  # 执行完提交
+            return True
+        except:
+            print("create table error")
+            return False
+        finally:
+            self.close()
+
+    # 删除表
+    def drop_table(self, sql):
+        self.connect()
+        try:
+            if 'DELETE' in sql.upper():
+                self._cursor.execute(sql)
+                self._conn.commit()
+                return True
+        except:
+            print('drop_table error')
+            return False
+        finally:
+            self.close()
+
+    # 查询所有符合条件数据，返回一个列表
+    def find_all(self, sql):
+        self.connect()
+        try:
+            self._cursor.execute(sql)
+            result = self._cursor.fetchall()
+            return result
+        except:
+            print('find_all error')
+            return False
+        finally:
+            self.close()
+
+    # 增删改
+    def update(self, sql):
+        self.connect()
+        try:
+            self._cursor.execute(sql)
+            self._conn.commit()
+            return True
+        except:
+            print('update error')
+            return False
+        finally:
+            self.close()
+
+
+
+
+
