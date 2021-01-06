a8fcb1f5abfa1f59c1f6d5c6ad241789344ff764
Truth <tideborn@foxmail.com>
2021-01-06 13:07:50 +0800

    进一步解除视图和控制器之间的耦合，避免控制器根据视图内的具体部件修改

--- controller/login_controller.py
+++ controller/login_controller.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore
 from view.login_view import LoginView
 from model.login_model import LoginModel
+from controller import home_controller,society_controller
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 import time
@@ -17,32 +18,29 @@ class LoginController():
 
     # 登录动作
     def click_btn_login(self):
-        # 获取视图login_view信息
-        input_username = self.view.line_input_username.text()
-        input_password = self.view.line_input_password.text()
-        env_society = self.view.radiobtn_society.isChecked()
-        env_home = self.view.radiobtn_home.isChecked()
-        if input_username == '' or input_password == '':  # 判空
-            self.view.label_tips.setText('请输入用户名和密码')#改变视图显示
-        elif env_home==False and env_society==False:
-            self.view.label_tips.setText('请选择工作环境')
-        else:# 向模型login_model发出信号
-            flag=self.model.try_to_login(input_username, input_password)
-            if flag==True:
-                self.view.label_tips.setText("登陆成功")
-                #time.sleep(1)
+        input_username = self.view.get_input_username()  # 获取视图login_view信息
+        input_password = self.view.get_input_password()
+        env_society = self.view.is_env_home_selected()
+        env_home = self.view.is_env_society_selected()
+        if input_username == '' or input_password == '':  # 输入判空
+            self.view.show_input_lack_tips()  # 改变视图显示
+        elif env_home == False and env_society == False:  # 环境选择判空
+            self.view.show_env_tips()
+        else:  # 向模型login_model发出信号
+            flag = self.model.try_to_login(input_username, input_password)
+            if flag == True:
+                self.view.show_login_ok_tips()
+                # time.sleep(1)
                 self.view.close()
             else:
-                self.view.label_tips.setText("用户名或密码错误")
-                #self.open_working_windows(env_society,env_home)
-    # def open_working_windows(self,env_society,env_home):
-    #     if env_home:
-    #         model=HomeModel()
-    #             new_working_windows=HomeController(model)
+                self.view.show_input_error_tips()
+                self.open_working_windows(env_society, env_home)
 
-
-
-
+    def open_working_windows(self, env_society, env_home):
+        if env_home == True:
+            new_working_window = home_controller.HomeController()
+        else:
+            new_working_window=society_controller.SocietyController()
 
 
 if __name__ == '__main__':
@@ -50,57 +48,3 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = LoginController()
     sys.exit(app.exec_())
-# from PyQt5 import QtCore
-#
-# from view.ui_login import Ui_login
-# from PyQt5.QtWidgets import QApplication,QMainWindow
-# import sys
-# import time
-# from db.operation import DatabaseVisitor
-#
-# class LoginController(QMainWindow,Ui_login):
-#     def __init__(self):
-#         QMainWindow.__init__(self)
-#         Ui_login.__init__(self)
-#         self.setupUi(self)
-#         self.btn_login.clicked.connect(lambda:self.click_btn_login())
-#
-#     #登录控制逻辑
-#     def click_btn_login(self):
-#         dbvisitor=DatabaseVisitor()
-#         username=self.input_username.text()
-#         password=self.input_password.text()
-#         if username=='' or password=='':#判空
-#             self.label_tips.setText('请输入用户名和密码')
-#         elif self.radiobtn_house.isChecked()==False and self.radiobtn_social.isChecked()==False:
-#             self.label_tips.setText('请选择工作环境')
-#         else:
-#             sql="SELECT Upassword FROM User WHERE Uname = '%s'" % (username)
-#             re=dbvisitor.find_one(sql)
-#             db_password=re[0]
-#             print(db_password)
-#             print(password)
-#             if db_password==False:
-#                 self.label_tips.setText('此管理员用户不存在')
-#             else:
-#                 if password==db_password:
-#                     self.label_tips.setText('登录成功')
-#                     time.sleep(1)
-#                     self.close()#关闭登录界面
-#                     self.open_working_window()#打开对应模式的工作界面
-#                 else:
-#                     self.label_tips.setText('密码错误')
-#
-#     #打开对应模式工作窗口
-#     def open_working_window(self):
-#         if self.radiobtn_social.isChecked()==True:
-#             pass
-#         else:
-#             pass
-#
-# if __name__ == '__main__':
-#     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-#     app = QApplication(sys.argv)
-#     ui = LoginControl()
-#     ui.show()
-#     sys.exit(app.exec_())
--- view/login_view.py
+++ view/login_view.py
@@ -8,10 +8,33 @@ class LoginView(QMainWindow,Ui_login):
         super(LoginView, self).__init__()
         self.setupUi(self)
 
+    def get_input_username(self):
+        return self.line_input_username.text()
+
+    def get_input_password(self):
+        return self.view.line_input_password.text()
+
+    def is_env_home_selected(self):
+        return self.view.radiobtn_home.isChecked()
+
+    def is_env_society_selected(self):
+        return self.view.radiobtn_society.isChecked()
+
+    def show_input_lack_tips(self):
+        self.label_tips.setText('请输入用户名和密码')
+
+    def show_env_tips(self):
+        self.label_tips.setText('请选择工作环境')
+
+    def show_login_ok_tips(self):
+        self.label_tips.setText("登陆成功")
+
+    def show_input_error_tips(self):
+        self.label_tips.setText("用户名或密码错误")
 
 if __name__ == '__main__':
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-    app = QApplication(sys.argv)  # 如果是启动文件，则创建 Application 实例 app
+    app = QApplication(sys.argv)  # 创建 Application 实例 app
     window = LoginView()
     window.show()                 # 显示窗口
     sys.exit(app.exec_())
