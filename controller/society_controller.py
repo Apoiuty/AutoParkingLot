d343f56384b182fc904b88ad7e606af65c387401
DaHuanjia <38311153+DaHuanjia@users.noreply.github.com>
2021-01-11 21:49:28 +0800

    society控制器针对手动模式修改

--- controller/society_controller.py
+++ controller/society_controller.py
@@ -13,8 +13,11 @@ class SocietyController():
         self.view = SocietyView()
         self.model = SocietyModel(self.view)
         self.view.setupUi(self.view)
-        self.view.btn_relogin.clicked.connect(lambda: self.click_btn_relogin())
+        self.view.btn_relogin.clicked.connect(lambda: self.click_btn_relogin())#绑定按钮的点击事件
+        self.view.btn_handmode_in.clicked.connect(lambda: self.click_btn_handmode_in())
+        self.view.btn_handmdoe_out.clicked.connect(lambda: self.click_btn_handmode_out())
         self.view.show()  # 控制器创建视图
+        self.view.open_auto_mode()  # 启动默认自动模式，手动模式组件不可见
         self.new_view = None
         self.rate = 0.05
         self.circumstance = 'society'
@@ -25,14 +28,28 @@ class SocietyController():
         self.view.action_9.triggered.connect(lambda: self.Log_menu('owner'))
         self.view.action_5.triggered.connect(self.Rate_set)
         # 菜单方法-模式切换绑定
+        self.view.action_hand_mode.triggered.connect(self.open_hand_mode)
+        self.view.action_auto_mode.triggered.connect(self.open_auto_mode)
 
-    def click_btn_relogin(self):  # 重新登录
+    def click_btn_relogin(self):  # 重新登录点击事件的控制
         re = self.view.ensure_to_quit()
         if re == True:
             self.view.close()
             qApp.exit(666)  # 666是重新登录代码
         else:
             pass
+
+    def click_btn_handmode_in(self):#手动模式下输入进入车牌按钮点击的控制
+        car_in=self.view.get_handmode_car_in()
+        #print(car_in)
+        self.view.clear_input_handmode_car_in()
+        pass#然后进入正常流程，后续流程和自动识别一样
+
+    def click_btn_handmode_out(self):#手动模式下输入离开车牌按钮点击的控制
+        car_out=self.view.get_handmode_car_out()
+        #print(car_out)
+        self.view.clear_input_handmode_car_out()
+        pass#然后进入正常流程，后续流程和自动识别一样
 
     def identify_in_car(self, img):
         self.view.show_in_img(img)
@@ -42,6 +59,11 @@ class SocietyController():
         self.view.show_out_img(img)
         self.model.identify_result(img, 1)  # 1是出标志
 
+    def open_hand_mode(self):#菜单栏触发的模式切换，改变视图组件显示
+        self.view.open_hand_mode()
+
+    def open_auto_mode(self):
+        self.view.open_auto_mode()
 
     def Rate_set(self):
         """
