1a6bbb5205c2ca54688e830e410b76d967d96d49
DaHuanjia <38311153+DaHuanjia@users.noreply.github.com>
2021-01-11 19:05:41 +0800

    流程优化

--- controller/society_controller.py
+++ controller/society_controller.py
@@ -1,30 +1,29 @@
-import sys
-
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, qApp
 
 from controller.Car_log_controller import LogController
-from model.society_model import SocietyModel
-from view.rate_view import rate_view
 from view.society_view import SocietyView
+from model.society_model import SocietyModel
+from PyQt5.QtWidgets import QApplication, qApp
+import sys
+from view.rate_view import rate_view
 
 
 class SocietyController():
     def __init__(self):
         self.view = SocietyView()
-        self.model = SocietyModel()
+        self.model = SocietyModel(self.view)
         self.view.setupUi(self.view)
         self.view.btn_relogin.clicked.connect(lambda: self.click_btn_relogin())
         self.view.show()  # 控制器创建视图
         self.new_view = None
         self.rate = 0.05
-        self.circumstance = 'society'
 
         # 菜单方法绑定
         self.view.action_4.triggered.connect(lambda: self.Log_menu('plate'))
         self.view.action_7.triggered.connect(lambda: self.Log_menu('time'))
         self.view.action_9.triggered.connect(lambda: self.Log_menu('owner'))
         self.view.action_5.triggered.connect(self.Rate_set)
+        # 菜单方法-模式切换绑定
 
     def click_btn_relogin(self):  # 重新登录
         re = self.view.ensure_to_quit()
@@ -35,16 +34,13 @@ class SocietyController():
             pass
 
     def identify_in_car(self, img):
-        result = self.model.identify_result(img)
-        self.view.show_in_car(result)  # 改变视图显示
-        self.view.show_in_time()
         self.view.show_in_img(img)
+        self.model.identify_result(img, 0)  # 0是进入标志
 
     def identify_out_car(self, img):
-        result = self.model.identify_result(img)
-        self.view.show_out_car(result)  # 改变视图显示
-        self.view.show_out_time()
         self.view.show_out_img(img)
+        self.model.identify_result(img, 1)  # 1是出标志
+
 
     def Rate_set(self):
         """
@@ -59,7 +55,7 @@ class SocietyController():
         :param mode:
         :return:
         """
-        log_dialog = LogController(self.circumstance, mode)
+        log_dialog = LogController(mode)
 
 
 if __name__ == '__main__':
