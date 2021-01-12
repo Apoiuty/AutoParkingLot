84657f61deb3df5aaee904d2b8e31267cd0e7b84
Apoiuty <2450320047@qq.com>
2021-01-12 16:02:29 +0800

    准备实现最终的出去流程。

--- view/home_view.py
+++ view/home_view.py
@@ -1,18 +1,20 @@
-from PyQt5.QtWidgets import QMainWindow,QApplication
-from PyQt5 import QtCore
-from view.pure_ui.home_ui import Ui_home
 import sys
 
-class HomeView(QMainWindow,Ui_home):
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from view.pure_ui.home_ui import Ui_home
+
+
+class HomeView(QMainWindow, Ui_home):
     def __init__(self):
         super(HomeView, self).__init__()
         self.setupUi(self)
-
 
 
 if __name__ == '__main__':
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QApplication(sys.argv)  # 创建 Application 实例 app
     window = HomeView()
-    window.show()                 # 显示窗口
+    window.show()  # 显示窗口
     sys.exit(app.exec_())
