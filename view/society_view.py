5c3579118a0f0add52dee2fdb1f08986f6dcafa9
DaHuanjia <148161616@qq.com>
2021-01-12 14:05:30 +0800

    society视图结合手动模式更新

--- view/society_view.py
+++ view/society_view.py
@@ -17,7 +17,7 @@ class SocietyView(QMainWindow,Ui_society):
             return False
 
     def msgbox_to_check_handmode_input(self):
-        reply = QMessageBox.question(self, 'Message', '请输入正确车牌号格式')
+        reply = QMessageBox.question(self, 'Message', '请输入正确车牌号格式',QMessageBox.Yes , QMessageBox.Yes)
 
     def show_in_car(self,result):
         self.label_in_car.setText(result)
@@ -44,6 +44,7 @@ class SocietyView(QMainWindow,Ui_society):
         self.label_out_img.setPixmap(jpg)
 
     def open_hand_mode(self):#切换手动模式
+        self.clear_view()
         self.label_in_img.setVisible(False)
         self.label_out_img.setVisible(False)
         self.line_handmode_car_in.setVisible(True)#模式切换时让手动模式组件可见
@@ -53,7 +54,9 @@ class SocietyView(QMainWindow,Ui_society):
         self.action_hand_mode.setEnabled(False)#菜单栏显示改变
         self.action_auto_mode.setEnabled(True)
 
+
     def open_auto_mode(self):#切换自动模式
+        self.clear_view()
         self.line_handmode_car_in.setVisible(False)#模式切换时让自动模式组件可见
         self.line_handmode_car_out.setVisible(False)
         self.btn_handmdoe_out.setVisible(False)
@@ -64,16 +67,28 @@ class SocietyView(QMainWindow,Ui_society):
         self.action_hand_mode.setEnabled(True)
 
     def get_handmode_car_in(self):#手动模式下提供用户输入的接口，屏蔽视图实现细节，被控制器调用
-        return self.line_handmode_car_in.text()
+        car=self.line_handmode_car_in.text()
+        return car
 
     def get_handmode_car_out(self):
-        return self.line_handmode_car_out.text()
+        car = self.line_handmode_car_out.text()
+        return car
 
     def clear_input_handmode_car_in(self):#输入后清除输入框，视图自身不调用，被控制器调用
         self.line_handmode_car_in.setText('')
 
     def clear_input_handmode_car_out(self):
         self.line_handmode_car_out.setText('')
+
+    def clear_view(self):#清空视图输入显示
+        self.label_in_img.clear()
+        self.label_out_img.clear()
+        self.label_in_car.clear()
+        self.label_out_car.clear()
+        self.label_in_time.clear()
+        self.label_out_time.clear()
+        self.label_fee.clear()
+
 
 
 
